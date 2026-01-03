0f71fd0484b4868052ed4f47162937370a18d58e
marla-sreevalli <marlasreevalli04@gmail.com>
2026-01-03 16:02:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
 import os
-from flask import Flask, render_template
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-app = Flask(
-    __name__,
-    template_folder=os.path.join(BASE_DIR, "templates"),
-    static_folder=os.path.join(BASE_DIR, "static")
-)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+print(os.getcwd())
+print(os.listdir())
+print(os.listdir("templates"))
